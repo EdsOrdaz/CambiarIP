9d36d31b355ace5f9db74d73319224579b808cfa
EdsOrdaz <edsordaz@hotmail.com>
2022-01-12 11:10:44 -0600

    Agregar archivos de proyecto.

---
+++ CambiarIP/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace CambiarIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.libre = new System.Windows.Forms.Button();
+            this.telmex = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // libre
+            // 
+            this.libre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.libre.Location = new System.Drawing.Point(12, 12);
+            this.libre.Name = "libre";
+            this.libre.Size = new System.Drawing.Size(79, 57);
+            this.libre.TabIndex = 0;
+            this.libre.Text = "16.252";
+            this.libre.UseVisualStyleBackColor = true;
+            this.libre.Click += new System.EventHandler(this.libre_Click);
+            // 
+            // telmex
+            // 
+            this.telmex.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.telmex.Location = new System.Drawing.Point(107, 12);
+            this.telmex.Name = "telmex";
+            this.telmex.Size = new System.Drawing.Size(79, 57);
+            this.telmex.TabIndex = 1;
+            this.telmex.Text = "18.253";
+            this.telmex.UseVisualStyleBackColor = true;
+            this.telmex.Click += new System.EventHandler(this.telmex_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 82);
+            this.Controls.Add(this.telmex);
+            this.Controls.Add(this.libre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cambiar IP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button libre;
+        private System.Windows.Forms.Button telmex;
+    }
+}
+
---
+++ CambiarIP/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace CambiarIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime hoy = DateTime.Now;
+            DateTime exp = DateTime.Parse("2022/01/13");
+            int result = DateTime.Compare(hoy, exp);
+
+            Console.WriteLine(result);
+
+            if(result > 0)
+            {
+                MessageBox.Show("Programa Expiro.", "Cambiar IP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        public void IP(string ip_address, string subnet_mask)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    try
+                    {
+                        ManagementBaseObject setIP;
+                        ManagementBaseObject newIP =
+                            objMO.GetMethodParameters("EnableStatic");
+
+                        newIP["IPAddress"] = new string[] { ip_address };
+                        newIP["SubnetMask"] = new string[] { subnet_mask };
+
+                        setIP = objMO.InvokeMethod("EnableStatic", newIP, null);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+
+
+                }
+            }
+        }
+
+        public void Puerta_de_Enlace(string gateway)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    try
+                    {
+                        ManagementBaseObject setGateway;
+                        ManagementBaseObject newGateway =
+                            objMO.GetMethodParameters("SetGateways");
+
+                        newGateway["DefaultIPGateway"] = new string[] { gateway };
+                        newGateway["GatewayCostMetric"] = new int[] { 1 };
+
+                        setGateway = objMO.InvokeMethod("SetGateways", newGateway, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al cambiar la puerta de enlace.\n\nError: "+ex.Message, "Cambiar IP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public void DNS(string NIC, string DNS)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["Caption"].Equals(NIC))
+                    {
+                        try
+                        {
+                            ManagementBaseObject newDNS =
+                                objMO.GetMethodParameters("SetDNSServerSearchOrder");
+                            newDNS["DNSServerSearchOrder"] = DNS.Split(',');
+                            ManagementBaseObject setDNS =
+                                objMO.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void libre_Click(object sender, EventArgs e)
+        {
+            Puerta_de_Enlace("172.16.16.252");
+            MessageBox.Show("Puerta de enlace cambiada.", "Cambiar IP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void telmex_Click(object sender, EventArgs e)
+        {
+            Puerta_de_Enlace("172.16.18.253");
+            MessageBox.Show("Puerta de enlace cambiada.", "Cambiar IP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
